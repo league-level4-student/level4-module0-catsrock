d3af14dcf51ed8ab5e83d7ce26c8f9381a76b137
League Student <league@wtsim17.attlocal.net>
2019-05-19 14:29:56 -0700

    Started Level 4 Mod 0
    Started Pixel Art
    Am currently drawing and filling in rectangle

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,29 +1,40 @@
 package _01_Crazy_Digital_Painting;
 
-public class CrazyDigitalPainting {
-	//1. Create two final static integers for the width and height of the display.
+import java.awt.Color;
 
-	
-	//2. Create a 2D array of Color objects. You will need to import
-	//java.awt.Color. Initialize the size of the array using the 
-	//integers created in step 1.
-	
-	
-	
+public class CrazyDigitalPainting {
+	// 1. Create two final static integers for the width and height of the display.
+	final static int width = 1000;
+	final static int height = 1038;
+
+	// 2. Create a 2D array of Color objects. You will need to import
+	// java.awt.Color. Initialize the size of the array using the
+	// integers created in step 1.
+	Color[][] array2d = new Color[width][height];
+
 	public CrazyDigitalPainting() {
-		//3. Open the crazy_digital_painting.png file and look at the image.
-		
-		//4. Iterate through the 2D array and initialize each Color object
-		//   to a new color. The sample image was created using the following 
-		//   pattern:
-		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-		
-		//5. Come up with your own pattern to make a cool crazy image.
-		
-		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
-		//   to show off your picture.
+		// 3. Open the crazy_digital_painting.png file and look at the image.
+
+		// 4. Iterate through the 2D array and initialize each Color object
+		// to a new color. The sample image was created using the following
+		// pattern:
+		// colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				array2d[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+			}
+		}
+		// 5. Come up with your own pattern to make a cool crazy image.
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				array2d[i][j] = new Color(i % 100, (i + j) % 100, j % 215);
+			}
+		}
+		// 6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method
+		// to show off your picture.
+		ColorArrayDisplayer.displayColorsAsImage(array2d);
 	}
-	
+
 	public static void main(String[] args) {
 		new CrazyDigitalPainting();
 	}
